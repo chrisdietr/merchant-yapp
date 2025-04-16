7440e8bc8de5cbe5d33015a36f629a6b82ed8947
beatsmeidk <tamam.elalti@gmail.com>
2025-04-16 22:12:56 +0200

    UI fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import '@rainbow-me/rainbowkit/styles.css';
-import "./styles/wallet-fixes.css";
 import { BrowserRouter } from "react-router-dom";
 
 import { TempoDevtools } from "tempo-devtools";
